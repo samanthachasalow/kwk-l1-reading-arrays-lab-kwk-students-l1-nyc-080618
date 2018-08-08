1f1060f7c5c69a3f751aa21498efb7320acc1d60
samanthachasalow <samanthachasalow@chatham-nj.org>
2018-08-08 18:02:39 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,11 +1,11 @@
 STUDENT_NAMES = ["Adele", "Beyoncé","Cardi B", "Lady Gaga", "Nicki Minaj","Rihanna"]
 
 def first_student_by_index
-puts STUDENT_NAMES.first
+puts STUDENT_NAMES[0]
 end
 
 def fourth_student_by_index
-  puts STUDENT_NAMES
+  puts STUDENT_NAMES[3]
 end
 
 def last_student_by_index
