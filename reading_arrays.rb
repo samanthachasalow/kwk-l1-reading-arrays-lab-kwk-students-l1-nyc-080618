7f9facb754b274fa69355d0cb87e5ee28a5a276a
samanthachasalow <samanthachasalow@chatham-nj.org>
2018-08-08 18:01:54 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -12,14 +12,14 @@ def last_student_by_index
   puts STUDENT_NAMES.last
 end
 
-def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
-end
+# def first_student_by_method
+#   # Write a solution that returns the first student using the built in .first method
+# end
 
-def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
-end
+# def last_student_by_method
+#   # Write a solution that returns the first student using the built in .last method
+# end
 
-def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
-end
+# def first_second_and_third_students
+#   # Write a solution that returns the first, second and third students
+# end
