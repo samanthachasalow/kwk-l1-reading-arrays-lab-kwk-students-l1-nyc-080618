8999ed64691fc84a0047c85d74678860d4bac967
samanthachasalow <samanthachasalow@chatham-nj.org>
2018-08-08 18:04:17 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -9,17 +9,17 @@ def fourth_student_by_index
 end
 
 def last_student_by_index
+  puts STUDENT_NAMES[5]
+end
+
+ def first_student_by_method
+  puts STUDENT_NAMES.first
+ end
+
+def last_student_by_method
   puts STUDENT_NAMES.last
 end
 
-# # def first_student_by_method
-# puts STUDENT_NAMES.first
-# # end
-
-# def last_student_by_method
-#   # Write a solution that returns the first student using the built in .last method
-# end
-
-# def first_second_and_third_students
-#   # Write a solution that returns the first, second and third students
-# end
+def first_second_and_third_students
+puts STUDENT_NAMES[0,1,2]
+end
