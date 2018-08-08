eca1a351370beca756371862a11158f936d109bf
samanthachasalow <samanthachasalow@chatham-nj.org>
2018-08-08 18:01:45 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,10 +1,4 @@
-STUDENT_NAMES = [
-  "Adele",
-  "Beyoncé",
-  "Cardi B",
-  "Lady Gaga",
-  "Nicki Minaj",
-  "Rihanna"
+STUDENT_NAMES = ["Adele", "Beyoncé","Cardi B", "Lady Gaga", "Nicki Minaj","Rihanna"
 ]
 
 def first_student_by_index
